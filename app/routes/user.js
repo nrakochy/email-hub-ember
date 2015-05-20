4c8d7d35f289c8782a7fd4865f460eb4fea88741
nrakochy <nick.rakochy@gmail.com>
2015-05-20 15:04:52 -0500

    Separated promise return on API call

--- app/routes/user.js
+++ app/routes/user.js
@@ -6,18 +6,21 @@ export default Ember.Route.extend({
       url: "https://api.github.com/users/" + params.username + "/events/public",
       type: "get"
     }).then(function(responses){
-      var emails = [];
       var records = [];
       responses.forEach(function(response){
         var recordArr = response.payload.commits;
         if (recordArr !== undefined && typeof(recordArr === Array)){
           records.push(recordArr[0]);
         }
-      });
-
+      })
+      return records
+    }).then(function(records){
+      var emails = [];
       records.forEach(function(record){
         var respObj = record.author.email;
+        /* Email exists */
         if (typeof(respObj !== undefined)){
+          /* Email is unique */
           if (emails.indexOf(respObj) === -1){
             emails.push(respObj);
           }
@@ -25,6 +28,6 @@ export default Ember.Route.extend({
       });
       console.log(emails);
       return emails;
-    });
+    })
   }
 });
